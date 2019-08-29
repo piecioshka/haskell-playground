c322dba1500588c01af1b35bcb86746867e6b55a
Piotr Kowalski <piecioshka@gmail.com>
2019-08-29 21:59:20 +0200

    Add tests calls

--- main.hs
+++ main.hs
@@ -1,11 +1,13 @@
-import System.IO
+import Data.List
+    -- "sort"
+-- import System.IO
 
 main = do
 
     let false = True
     let string = "ccc";
 
-    let s = sum [1..1000]
+    let s = sum [1, 3..1000]
     print "s";
     print s;
 
@@ -30,18 +32,46 @@ main = do
     print (list !! 1) -- list element by index
 
     print "(head list)"
-    print (head list)
+    print (head list) -- return first value from list
 
     print "(last list)"
-    print (last list)
+    print (last list) -- return last value form list
 
     print "(init list)"
-    print (init list)
+    print (init list) -- return item without last value
 
     print "(take 2 list)"
-    print (take 2 list)
+    print (take 2 list) -- returns first 2 items
 
+    print "(20 `elem` list)"
+    print (20 `elem` list) -- check is value inside list
 
+    print "(maximum list)"
+    print (maximum list)
 
+    print "(minimum list)"
+    print (minimum list)
 
+    print ['a', 'b'..'z']
+    print (['a', 'b'..'z'] !! 3)
 
+    -- infinity list
+    print "(take 50 [1, 2..])"
+    print (take 50 [1, 2..])
+    -- print ([1, 2..])
+
+    print (take 5 (repeat 4))
+    print (replicate 5 4) -- the same as above
+
+    print "(take 10 (cycle [1, 2, 3]))"
+    print (take 10 (cycle [1, 2, 3]))
+
+    print "( [x * 3 | x <- list] )"
+    print ( [x * 3 | x <- list] ) -- map
+
+    print "( [x | x <- list, x `mod` 2 == 0 ] )"
+    print ( [x | x <- list, x `mod` 2 == 0 ] ) -- filter
+
+    print ( [x | x <- [1..1000], x `mod` 13 == 0, x `mod` 27 == 0] ) -- couple filters
+
+    print (sort [3, 1, 4])
